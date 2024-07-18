d3f428f8c55a5be26c2651c1d7b692fd42a330af
Sergio López Rey <sergiolopezdanielrey@gmail.com>
2024-07-17 22:00:22 -0300

    prueba

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,28 @@ import { ChakraProvider } from '@chakra-ui/react'
 import NavBar from './components/NavBar/NavBar'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
-import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css'
 import PageNotFound from './components/PageNotFound/PageNotFound'
 import { CartContextProvider } from './context/CartContext'
 import Cart from './components/Cart/Cart'
 import Checkout from './components/Checkout/Checkout'
 
 function App() {
-
   return (
     <ChakraProvider>
       <CartContextProvider>
         <BrowserRouter>
           <NavBar />
           <Routes>
-            <Route path='/' element={<ItemListContainer title='Tienda' />} />
-            <Route path='/categoria/:categoryId' element={<ItemListContainer title='Tienda'/>} />
-            <Route path='/producto/:productId' element={<ItemDetailContainer />} />
-            <Route path='/cart' element={<Cart />} />
-            <Route path='/checkout' element={<Checkout />} />
-            <Route path='*' element={<PageNotFound />} />
+            <Route path="/" element={<ItemListContainer title="Tienda" />} />
+            <Route path="/categoria/:categoryId" element={<ItemListContainer title="Tienda" />} />
+            <Route path="/producto/:productId" element={<ItemDetailContainer />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
           </Routes>
         </BrowserRouter>
       </CartContextProvider>
     </ChakraProvider>
-
   )
 }
 
